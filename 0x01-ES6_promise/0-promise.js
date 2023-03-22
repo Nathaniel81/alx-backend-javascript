07187649ad8f38469217f7b2648af953347de8e7
Nathaniel81 <nathanield356@gmail.com>
2023-03-22 14:47:13 +0300

    Anthony Hitt

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export defualt function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		resolve('I hate promises')
-	});
+  return new Promise((resolve) => {
+    resolve('I hate promises');
+  });
 }
