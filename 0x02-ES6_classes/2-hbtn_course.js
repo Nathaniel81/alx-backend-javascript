07a1bf8c25b711b537e45994706fac7fccabea72
Nathaniel81 <104891061+Nathaniel81@users.noreply.github.com>
2023-03-23 21:25:48 +0300

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,41 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name
-    this.length = length
-    this.students = students
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
-    return this._name
+    return this._name;
   }
 
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
     } else {
-      throw new TypeError('Name must be a string')
+      throw new TypeError('Name must be a string');
     }
   }
 
   get length() {
-    return this._length
+    return this._length;
   }
 
   set length(len) {
     if (typeof len === 'number') {
-      this._length = len
+      this._length = len;
     } else {
-      throw new TypeError('Length must be a number')
+      throw new TypeError('Length must be a number');
       }
     }
 
   get students() {
-    return this._students
+    return this._students;
   }
 
   set students(val) {
     if (Array.isArray(val) && val.every(item => typeof item === 'string')) {
-      this._students = val
+      this._students = val;
     } else {
       throw new TypeError("Students must be an array of strings");
       }
