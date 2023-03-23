37a3109497199fa295c171dec0533cbbb3a7c16c
Nathaniel81 <nathanield356@gmail.com>
2023-03-23 20:08:35 +0300

    classrooms

--- 0x02-ES6_classes/1-make_classrooms2.js
+++ 0x02-ES6_classes/1-make_classrooms2.js
@@ -1,5 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  const sizes = [19, 20, 34];
+  return sizes.map(s => new ClassRoom(s));
 }
