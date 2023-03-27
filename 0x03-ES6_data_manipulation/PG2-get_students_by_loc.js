a20fa7c9c3ebd02b7a343b014ec617286010801e
Nathaniel81 <nathanield356@gmail.com>
2023-03-27 11:59:00 +0300

    Filter

---
+++ 0x03-ES6_data_manipulation/PG2-get_students_by_loc.js
@@ -0,0 +1,13 @@
+const getListStudents = require('./PG_0-get_list_students')
+
+function getStudentsByLocation(listOfStd, city) {
+	// const filteredArray = listOfStd.filter((o) => {
+	// 	return o.location === city;
+	// })
+	// return filteredArray;
+	return listOfStd.filter(o => o.location === city)
+}
+
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco'));
