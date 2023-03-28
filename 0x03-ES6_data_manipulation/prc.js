91a5044335678a595028df260319c428537e84a3
Nathaniel81 <nathanield356@gmail.com>
2023-03-28 12:48:02 +0300

    Weak link data structure

--- 0x03-ES6_data_manipulation/prc.js
+++ 0x03-ES6_data_manipulation/prc.js
@@ -8,9 +8,36 @@
 	// Remove the trailing "-" character from the end
 // 	return result.slice(0, -1);
 //   }
-const a = new Map();
 
-a.set('ss', 5);
-a.set('tt', 77);
+// const person = new WeakMap();
+// const ff = {'f': 3};
+// person.set(ff, 8);
 
-a.forEach((aa, bb) => {console.log(aa, bb)});
+// console.log(person.get(ff));
+
+const maxcalls = 5;
+const weakMap = new WeakMap()
+
+function queryAPI(endpoint) {
+	if (!weakMap.has(endpoint)) {
+		weakMap.set(endpoint, 0);
+	}
+	weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+}
+
+
+
+const endpoint = { protocol: 'http', name: 'getUsers' };
+weakMap.get(endpoint);
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
