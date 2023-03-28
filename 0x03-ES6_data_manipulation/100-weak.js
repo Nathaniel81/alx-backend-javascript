bb92611625569816f6c91a864b42585cf6cc601a
Nathaniel81 <nathanield356@gmail.com>
2023-03-28 22:04:47 +0300

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+const weakMap = new WeakMap();
+const maxCalls = 5;
+
+export default function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= maxCalls) {
+    throw new Error('Endpoint load is high');
+  }
+}
