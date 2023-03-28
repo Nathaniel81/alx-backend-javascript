bedf15b8c148d4694b85216c483cafddea37c169
Nathaniel81 <nathanield356@gmail.com>
2023-03-28 22:13:39 +0300

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(mapobj) {
+  if (!(mapobj instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   mapobj.forEach((val, key) => {
     if (val === 1) {
       mapobj.set(key, 100);
