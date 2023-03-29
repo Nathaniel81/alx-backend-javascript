685758495cf0f84cc9faf3b050bd0ca9dc6d2e65
Nathaniel81 <nathanield356@gmail.com>
2023-03-29 22:04:35 +0300

    Upload

---
+++ 0x04-TypeScript/_task_0/dist/prc.js
@@ -0,0 +1,26 @@
+var student1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 21,
+    location: "New York",
+};
+var student2 = {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 19,
+    location: "Los Angeles",
+};
+var studentsList = [student1, student2];
+var table = document.createElement("table");
+document.body.appendChild(table);
+studentsList.forEach(function (student) {
+    var row = document.createElement("tr");
+    var firstNameCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+    row.appendChild(firstNameCell);
+    var locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+    table.appendChild(row);
+});
+//# sourceMappingURL=prc.js.map
